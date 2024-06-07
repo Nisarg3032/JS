2cf6b38c94d23fdcd7ef6420ed0eff3ca764ed55
Nisarg3032 <105769839+Nisarg3032@users.noreply.github.com>
2024-06-07 12:07:27 +0000

    memory explanation

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -2,33 +2,57 @@
 
 // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+const score = 100;
+const scoreValue = 100.3;
 
-const isLoggedIn = false
-const outsideTemp = null
+const isLoggedIn = false;
+const outsideTemp = null;
 let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+const id = Symbol("123");
+const anotherId = Symbol("123");
 
 console.log(id === anotherId);
 
-const bigNumber = 4895048594836986986909n
-
+const bigNumber = 4895048594836986986909n;
 
 // Reference (Non-Primitive)
 
 // Array, Objects, Functions
 
-const heros = ["Iron-man", "Spider-man", "Thor"]
+const heros = ["Iron-man", "Spider-man", "Thor"];
 let myObj = {
-    name:"nisarg",
-    age: 26,
-}
+  name: "nisarg",
+  age: 26,
+};
 
-const myFun = function(){
-    console.log("Hello World");
-}
+const myFun = function () {
+  console.log("Hello World");
+};
 
 console.log(typeof anotherId);
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// (Stack (Primitive), Heap (Non-Primitive)) Memory
+
+let myYoutubename = "PokeAxel"
+
+let anotherName = myYoutubename
+anotherName = "CodeAxel"
+
+console.log(myYoutubename);
+console.log(anotherName);
+
+let userOne = {
+    email : "user@google.com",
+    upi : "user@ybl",
+}
+
+let userTwo = userOne
+
+userTwo.email = "nisarg@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
